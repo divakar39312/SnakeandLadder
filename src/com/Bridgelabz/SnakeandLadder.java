d9caf0542f2f366cde51d180d996e76dd6c333c0
Divakar <divakar39312@gmail.com>
2022-08-04 16:07:10 +0530

    [Divakar]:dice was played to win game

--- src/com/Bridgelabz/SnakeandLadder.java
+++ src/com/Bridgelabz/SnakeandLadder.java
@@ -58,46 +58,46 @@ public class SnakeandLadder {
 			Scanner s = new Scanner(System.in);
 			String str;
 			int diceValue =0;
-//			do
-//			{
-//				System.out.println(currentPlayer==-1?"\n\nFIRST PLAYER TURN":"\n\nSECOND PLAYER TURN");
-//				System.out.println("Press r to roll Dice");
-//				str = s.next();
-//				diceValue = rollDice();
+			do
+			{
+				System.out.println(currentPlayer==-1?"\n\nFIRST PLAYER TURN":"\n\nSECOND PLAYER TURN");
+				System.out.println("Press r to roll Dice");
+				str = s.next();
+				diceValue = rollDice();
 				
 				
-//				if(currentPlayer == -1)
-//				{
-//					player1 = calculatePlayerValue(player1,diceValue);
-//					System.out.println("First Player :: " + player1);
-//					System.out.println("Second Player :: " + player2);
-//					System.out.println("------------------");
-//					if(isWin(player1))
-//					{
-//						System.out.println("First player wins");
-//						return;
-//					}
-//				}
-//				else
-//				{
-//					player2 = calculatePlayerValue(player2,diceValue);
-//					System.out.println("First Player :: " + player1);
-//					System.out.println("Second Player :: " + player2);
-//					System.out.println("------------------");
-//					if(isWin(player2))
-//					{
-//						System.out.println("Second player wins");
-//						return;
-//					}
-//				}
-//				
-//				currentPlayer= -currentPlayer;
-//				
-//				
-//				
-//			}while("r".equals(str));
-//		}
-//		
+				if(currentPlayer == -1)
+				{
+					player1 = calculatePlayerValue(player1,diceValue);
+					System.out.println("First Player :: " + player1);
+					System.out.println("Second Player :: " + player2);
+					System.out.println("------------------");
+					if(isWin(player1))
+					{
+						System.out.println("First player wins");
+						return;
+					}
+				}
+				else
+				{
+					player2 = calculatePlayerValue(player2,diceValue);
+					System.out.println("First Player :: " + player1);
+					System.out.println("Second Player :: " + player2);
+					System.out.println("------------------");
+					if(isWin(player2))
+					{
+						System.out.println("Second player wins");
+						return;
+					}
+				}
+				
+				currentPlayer= -currentPlayer;
+				
+				
+				
+			}while("r".equals(str));
+		}
+		
 		
 		public int calculatePlayerValue(int player, int diceValue)
 		{
@@ -123,10 +123,10 @@ public class SnakeandLadder {
 			return player;
 		}
 		
-//		public boolean isWin(int player)
-//		{
-//			return WINPOINT == player;
-//		}
-//		
-//	}
+		public boolean isWin(int player)
+		{
+			return WINPOINT == player;
+		}
+		
+	}
 
