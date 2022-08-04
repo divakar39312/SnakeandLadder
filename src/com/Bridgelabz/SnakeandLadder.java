d008650cfbbe00c16756efcb69aba57ba77b08f2
Divakar <divakar39312@gmail.com>
2022-08-04 15:59:17 +0530

    [Divakar]:UC5-added

--- src/com/Bridgelabz/SnakeandLadder.java
+++ src/com/Bridgelabz/SnakeandLadder.java
@@ -99,30 +99,30 @@ public class SnakeandLadder {
 //		}
 //		
 		
-//		public int calculatePlayerValue(int player, int diceValue)
-//		{
-//			player = player + diceValue;
-//			
-//			if(player > WINPOINT)
-//			{
-//				player = player - diceValue;
-//				return player;
-//			}
-//			
-//			if(null!=snake.get(player))
-//			{
-//				System.out.println("swallowed by snake");
-//				player= snake.get(player);
-//			}
-//			
-//			if(null!=ladder.get(player))
-//			{
-//				System.out.println("climb up the ladder");
-//				player= ladder.get(player);
-//			}
-//			return player;
-//		}
-//		
+		public int calculatePlayerValue(int player, int diceValue)
+		{
+			player = player + diceValue;
+			
+			if(player > WINPOINT)
+			{
+				player = player - diceValue;
+				return player;
+			}
+			
+			if(null!=snake.get(player))
+			{
+				System.out.println("swallowed by snake");
+				player= snake.get(player);
+			}
+			
+			if(null!=ladder.get(player))
+			{
+				System.out.println("climb up the ladder");
+				player= ladder.get(player);
+			}
+			return player;
+		}
+		
 //		public boolean isWin(int player)
 //		{
 //			return WINPOINT == player;
